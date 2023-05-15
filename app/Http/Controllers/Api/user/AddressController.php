8fc40d4c93f161f812a89f5e4217e6254ae744e5
Mohammed Hassan <medo.hassan0100@gmail.com>
2023-05-15 11:48:35 +0300

    addresses

--- app/Http/Controllers/Api/user/AddressController.php
+++ app/Http/Controllers/Api/user/AddressController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Api\user;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\AddressResource;
 use App\Models\Address;
-use App\Models\User;
+use App\Models\Client;
 use Illuminate\Http\Request;
 
 class AddressController extends Controller
