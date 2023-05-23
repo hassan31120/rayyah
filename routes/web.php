5d28b0f419b59af01556bbcefa7f152407f0c2d0
Mohammed Hassan <medo.hassan0100@gmail.com>
2023-05-23 13:54:18 +0300

    web

--- routes/web.php
+++ routes/web.php
@@ -104,12 +104,12 @@ Route::middleware('auth')->group(function () {
         Route::delete('settings/complain/delete/{id}', 'destroy')->name('complain.destroy');
     });
 
-    Route::controller(NotiController::class)->group(function(){
+    Route::controller(NotiController::class)->group(function () {
         Route::get('noti', 'noti')->name('noti');
         Route::post('send_noti', 'send_noti')->name('send_noti');
     });
 
-    Route::controller(SettingsController::class)->group(function(){
+    Route::controller(SettingsController::class)->group(function () {
         Route::get('settings', 'index')->name('settings');
     });
 
@@ -117,20 +117,20 @@ Route::middleware('auth')->group(function () {
     Route::get('/{page}', [AdminController::class, 'index']);
     Route::get('/{page}/edit', [AdminController::class, 'edit'])->name('profile.edit');
     Route::post('/{page}/update/{id}', [AdminController::class, 'update']);
-});
 
-Route::controller(OrderController::class)->group(function () {
-    Route::get('orders', 'index')->name('orders');
-    Route::get('orders/{id}', 'show')->name('show.orders');
-    Route::get('/invoice/{orderId}/generate', 'generateInvoice');
-    Route::get('MarkAsRead_all', 'MarkAsRead_all')->name('MarkAsRead_all');
-    Route::get('unreadNotifications_count', 'unreadNotifications_count')->name('unreadNotifications_count');
-    Route::get('unreadNotifications', 'unreadNotifications')->name('unreadNotifications');
-    Route::post('/notifications/{notification}/mark-as-read', 'markAsRead')->name('notifications.markAsRead');
-});
+    Route::controller(OrderController::class)->group(function () {
+        Route::get('orders', 'index')->name('orders');
+        Route::get('orders/{id}', 'show')->name('show.orders');
+        Route::get('/invoice/{orderId}/generate', 'generateInvoice');
+        Route::get('MarkAsRead_all', 'MarkAsRead_all')->name('MarkAsRead_all');
+        Route::get('unreadNotifications_count', 'unreadNotifications_count')->name('unreadNotifications_count');
+        Route::get('unreadNotifications', 'unreadNotifications')->name('unreadNotifications');
+        Route::post('/notifications/{notification}/mark-as-read', 'markAsRead')->name('notifications.markAsRead');
+    });
 
-Route::controller(TransactionController::class)->group(function () {
-    Route::get('transactions', 'index')->name('users.index');
+    Route::controller(TransactionController::class)->group(function () {
+        Route::get('transactions', 'index')->name('users.index');
+    });
 });
 
 require __DIR__ . '/auth.php';
