df918e40bd84ce8943841f082031a773d8e4edd1
mahmoud-hamed <ma.smeda2@gmail.com>
2023-05-28 11:26:57 +0300

    ramadan-res2

--- app/Http/Resources/TrackOrderResource.php
+++ app/Http/Resources/TrackOrderResource.php
@@ -18,15 +18,19 @@ class TrackOrderResource extends JsonResource
         return [
             'id'=>$this->id,
             'delivery' => $this->delivery->name ?? null,
-            'service_price'=>$this->total_service_price,
-            'delivery_price'=>$this->total_del_price,
-            'total_price'=>$this->total_cost,
+            'service_price'=>number_format($this->total_service_price),
+            'delivery_price'=>number_format($this->total_del_price),
+            'total_price'=>number_format($this->total_cost),
             'reference number'=> $this->ref_number,
             'service'=>$this->service->name ?? null,
             'address'=>$this->address->title,
             'description'=>$this->description,
             'status'=>$this->status,
-            'payment_method'=>$this->payment_method
+            'payment_method'=>$this->payment_method,
+            'date'=>$this->created_at,
+            'user'=>$this->client->name,
+            'user_image'=>asset($this->client->attachmentRelation[0]->path),
+            'phone'=>$this->client->number,
 
            
 
