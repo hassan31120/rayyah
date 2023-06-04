904ce84836e4882ad09c0f5a00f9f4132a4fcafb
Mohammed Hassan <medo.hassan0100@gmail.com>
2023-06-04 13:00:32 +0300

    cron job

---
+++ app/Console/Commands/ClearCacheCommand.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Cache;
+
+class ClearCacheCommand extends Command
+{
+    protected $signature = 'cache:clear';
+
+    protected $description = 'Clear the application cache';
+
+    public function handle()
+    {
+        Cache::flush();
+        $this->info('Application cache cleared.');
+    }
+}
